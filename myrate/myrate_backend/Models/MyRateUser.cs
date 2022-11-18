18018490e8ab30ff1f994dceff51e73f93ba3f80
nicholasgonzalez5 <nagonzalez8530@gmail.com>
2022-11-18 03:21:29 -0700

    Updated Header Comment and testing pushing with gitlab

--- myrate/myrate_backend/Models/MyRateUser.cs
+++ myrate/myrate_backend/Models/MyRateUser.cs
@@ -1,13 +1,13 @@
 ﻿/**
- * Authors:    Alyse  Palsulich, Nicholas Gonzalez
+ * Authors:   Alyse  Palsulich, Nicholas Gonzalez
  * Date:      11-16-2022
- * Course:    CS 4540, University of Utah, School of Computing
- * Copyright: CS 4540 and Alyse Palsulich and Nicholas Gonzalez - This work may not be copied for use in Academic Coursework.
+ * Course:    CS 4500, University of Utah, School of Computing
+ * Copyright: CS 4500 and Alyse Palsulich, Nicholas Gonzalez, Justin Springborn, and Rosemary Yoo - This work may not be copied for use in Academic Coursework.
  *
- * I, Alyse Palsulich and Nicholas Gonzalez, certify that I wrote this code from scratch and did 
- * not copy it in part or whole from another source.  Any references used 
- * in the completion of the assignment are cited in my README file and in
- * the appropriate method header.
+ * I, Alyse Palsulich, Nicholas Gonzalez, Justin Springborn, and Rosemary Yoo,
+ * certify that I wrote this code from scratch and did not copy it in part or 
+ * whole from another source. Any references used in the completion of the assignment 
+ * are cited in my README file and in the appropriate method header.
  *
  * File Contents
  * Model class for MyRate Users. Extends IdentitiyUser class.
