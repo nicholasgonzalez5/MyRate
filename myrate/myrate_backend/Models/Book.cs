55a9f76930d1c4307238826a7c71bfbbc62b80f2
nicholasgonzalez5 <nagonzalez8530@gmail.com>
2022-11-18 19:07:43 -0700

    Updated Book Model

--- myrate/myrate_backend/Models/Book.cs
+++ myrate/myrate_backend/Models/Book.cs
@@ -1,23 +1,27 @@
-﻿/**
- * Authors:   Alyse  Palsulich, Nicholas Gonzalez
- * Date:      11-16-2022
- * Course:    CS 4500, University of Utah, School of Computing
- * Copyright: CS 4500 and Alyse Palsulich, Nicholas Gonzalez, Justin Springborn, and Rosemary Yoo - This work may not be copied for use in Academic Coursework.
- *
- * I, Alyse Palsulich, Nicholas Gonzalez, Justin Springborn, and Rosemary Yoo,
- * certify that I wrote this code from scratch and did not copy it in part or 
- * whole from another source. Any references used in the completion of the assignment 
- * are cited in my README file and in the appropriate method header.
- *
- * File Contents
- * Model class for Books. Extends Media class.
- */
+﻿
+using System.ComponentModel.DataAnnotations;
+/**
+* Authors:   Alyse  Palsulich, Nicholas Gonzalez
+* Date:      11-16-2022
+* Course:    CS 4500, University of Utah, School of Computing
+* Copyright: CS 4500 and Alyse Palsulich, Nicholas Gonzalez, Justin Springborn, and Rosemary Yoo - This work may not be copied for use in Academic Coursework.
+*
+* I, Alyse Palsulich, Nicholas Gonzalez, Justin Springborn, and Rosemary Yoo,
+* certify that I wrote this code from scratch and did not copy it in part or 
+* whole from another source. Any references used in the completion of the assignment 
+* are cited in my README file and in the appropriate method header.
+*
+* File Contents
+* Model class for Books. Extends Media class.
+*/
 namespace myrate_backend.Models
 {
     public class Book
     {
-        public string Author { get; set; }
-        public string Summary { get; set; }
+        [Display(Name = "Author")]
+        public string? Author { get; set; }
+        [Display(Name = "Summary of Book")]
+        public string? Summary { get; set; }
         // In future we will add more properties such as tags
     }
 }
