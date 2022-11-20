6c5be9ba0ccffeb3ec21c092d6fc163112f0dff7
Mary <rsy9807@gmail.com>
2022-11-20 15:41:43 -0700

    Create Discover js file

---
+++ myrate/myrate_frontend/src/Screens/Discover.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Navbar from "../Components/Navbar";
+
+const Discover = () => {
+    return (
+        <>
+            <Navbar />
+            <h3>Discover Landing Page</h3>
+            <div>
+                <h2> Trending </h2>
+                <h2> Recommendations </h2>
+            </div>
+        </>
+    );
+};
+
+export default Discover;
