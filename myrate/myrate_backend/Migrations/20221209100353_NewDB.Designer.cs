cd509b5bf208a600a1c9342f57d170f3486efe29
nicholasgonzalez5 <nagonzalez8530@gmail.com>
2022-12-09 03:04:38 -0700

    Added Migrations to update the DB

--- myrate/myrate_backend/Migrations/20221209100353_NewDB.Designer.cs
+++ myrate/myrate_backend/Migrations/20221209100353_NewDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using myrate_backend.Data;
 
@@ -11,9 +12,10 @@ using myrate_backend.Data;
 namespace myrate_backend.Migrations
 {
     [DbContext(typeof(MyRateDbContext))]
-    partial class MyRateDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221209100353_NewDB")]
+    partial class NewDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
