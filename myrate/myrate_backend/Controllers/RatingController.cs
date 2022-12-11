0a63c381a158e53286701e0f98489dffe82ce2a4
Mary <rsy9807@gmail.com>
2022-12-11 14:19:21 -0700

    Post reviews

--- myrate/myrate_backend/Controllers/RatingController.cs
+++ myrate/myrate_backend/Controllers/RatingController.cs
@@ -1,17 +1,50 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using myrate_backend.Data;
 using myrate_backend.Models;
+using System.Text.Json.Serialization;
 
 namespace myrate_backend.Controllers
 {
+
+    public class ReviewDetails
+    {
+        [JsonPropertyName("stars")]
+        public int stars { get; set; }
+
+        [JsonPropertyName("review")]
+        public string review { get; set; }
+    }
+
+    [EnableCors("AllowAllOrigins")]
+    [Route("api/[controller]/[action]/")]
     public class RatingController : Controller
     {
         private MyRateDbContext _context;
         public RatingController(MyRateDbContext context)
         {
             _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SaveReview([FromBody] ReviewDetails details)
+        {
+            //TODO: check for existing reviews of this user and update
+            //var existingReview = _context.Ratings.SingleOrDefault
+
+            // otherwise add review to db
+            Rating newRating = Activator.CreateInstance<Rating>(); ;
+            newRating.Stars = details.stars;
+            newRating.Review = details.review;
+
+            var user = _context.Users.First();
+            newRating.User = user;
+            _context.Add(newRating);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { success = true });
         }
 
         /*
