7eeeb51e71ced824763ab144e46af491897fe599
Alyse Palsulich <brpalsulich@gmail.com>
2023-02-03 19:43:19 -0700

    Added seeding for ratings for books, movies, tvshows

--- myrate/myrate_backend/server/db/conn.js
+++ myrate/myrate_backend/server/db/conn.js
@@ -1,26 +1,55 @@
 const { MongoClient } = require("mongodb");
+const faker = require("faker");
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
- 
+
 var _db;
- 
+
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
       // Verify we got a good "db" object
-      if (db)
-      {
+      if (db) {
         _db = db.db("media");
-        console.log("Successfully connected to MongoDB."); 
+        console.log("Successfully connected to MongoDB.");
+
+        // seed ratings for media
+        var savedBooks = _db.collection("books").find().forEach(i => SeedReviews(i, "books"));
+        var savedMovies = _db.collection("movies").find().forEach(i => SeedReviews(i, "movies"));
+        var savedTVShows = _db.collection("tvshows").find().forEach(i => SeedReviews(i, "tvshows"));
+        console.log("Seeded ratings");
       }
       return callback(err);
-         });
+    });
   },
- 
+
   getDb: function () {
     return _db;
   },
 };
+
+function SeedReviews(media, type) {
+  const ratingscollection = client.db("media").collection("ratings");
+  let ratingData = [];
+  if (ratingscollection.find({ "media_id": media._id }) != null) {
+    return;
+  }
+  for (let i = 0; i < 2; i++) {
+    let newRating = {
+      stars: faker.datatype.number({
+        'min': 1,
+        'max': 5
+      }),
+      review: faker.lorem.paragraph(),
+      media_type: type,
+      media_id: media._id
+    };
+
+    ratingData.push(newRating);
+  }
+  ratingscollection.insertMany(ratingData);
+
+}
